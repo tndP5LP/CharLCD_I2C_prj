3f7fab8e553e6ab801d31f72718a21da976549be
noeldiviney <noel@ayrenet.com>
2017-12-05 09:34:51 +1100

    updating CharLCD_I2C_Test

--- CharLCD_I2C_Test.cydsn/main.c
+++ CharLCD_I2C_Test.cydsn/main.c
@@ -9,18 +9,58 @@
  *
  * ========================================
 */
-#include "project.h"
+#include <project.h>
 
-int main(void)
+cystatus I2C_status;
+
+_Bool FlgLCD_ERR;
+
+int main()
 {
-    CyGlobalIntEnable; /* Enable global interrupts. */
-
-    /* Place your initialization/startup code here (e.g. MyInst_Start()) */
-
-    for(;;)
+    
+	CyGlobalIntEnable; /* Enable global interrupts. */
+	
+	I2C_M_FF_Start();
+	CharLCD_PCF8574_I2C_Start();
+	
+	for(;;)
     {
-        /* Place your application code here. */
-    }
+/*
+		======================================
+		| ** CHECK LCD DISPLAY CONNECTION ** |
+		======================================
+*/
+		I2C_status = I2C_M_FF_MasterStatus();
+		
+		if ((I2C_status != I2C_M_FF_MSTAT_WR_CMPLT) && !FlgLCD_ERR){FlgLCD_ERR = 1;}
+	
+		if (FlgLCD_ERR && (I2C_status == I2C_M_FF_MSTAT_WR_CMPLT)) {
+			CyDelay(250u);
+			CharLCD_PCF8574_I2C_Init();
+			CyDelay(250u);
+			FlgLCD_ERR = 0;}
+		CyDelay(1u);			
+		
+		I2C_M_FF_MasterClearStatus();		
+/*
+		=============================================
+		| ** END of LCD DISPLAY CONNECTION CHECK ** |
+		=============================================
+
+		
+		=====================================================================
+		| ** Code Works with either 2x16 LCD Display or 4x20 LCD Display ** |
+		=====================================================================
+		
+*/
+		CharLCD_PCF8574_I2C_Position(0u,1u);
+		CharLCD_PCF8574_I2C_PrintString("Cypress PSoC 4");
+		//CharLCD_PCF8574_I2C_Position(1u,2u);
+		CharLCD_PCF8574_I2C_PosPrintString(1u,2u,"Hello World");
+		CharLCD_PCF8574_I2C_PosPrintString(2u,0u,"CY8CKIT-042 20x4 LCD");
+		CharLCD_PCF8574_I2C_PosPrintString(3u,0u,"DEMO of CharLCD_PCF8574_I2C");
+   		CyDelay(1u);
+	}
 }
 
 /* [] END OF FILE */
